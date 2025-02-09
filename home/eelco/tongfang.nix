6d42b9f5cf6c101442d2b235eea97b110fe03b5d
Eelco van Vlietl <eelco@davelab.nl>
2025-02-09 18:39:10 +0100

    added host to homes

---
+++ home/eelco/tongfang.nix
@@ -0,0 +1,23 @@
+{pkgs, ...}: {
+  imports = [
+    ./global
+    ./features/desktop/hyprland
+    ./features/desktop/wireless
+    ./features/productivity
+    ./features/pass
+    ./features/games
+  ];
+
+  # Purple
+  wallpaper = pkgs.wallpapers.aenami-lost-in-between;
+
+  monitors = [
+    {
+      name = "eDP-1";
+      width = 2560;
+      height = 1600;
+      workspace = "1";
+      primary = true;
+    }
+  ];
+}
